2f014218eea62ddf989d4d3a7b8105c275159acf
Tung51056 <tung51056@donga.edu.vn>
2024-01-21 21:41:55 +0700

    fix error 2

--- routes/trashCan.js
+++ routes/trashCan.js
@@ -3,19 +3,19 @@ const router = express.Router();
 const { trashCans, createTrashCan, updateTrashCan, addImage, getImage } = require('../controller/trashCan');
 const multer = require('multer');
 const path = require('path');
-const storage = multer.diskStorage({
-    destination: 'tmp/images/',
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    },
-});
+// const storage = multer.diskStorage({
+//     destination: 'tmp/images/',
+//     filename: function (req, file, cb) {
+//         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+//     },
+// });
 
-const upload = multer({ storage: storage });
+// const upload = multer({ storage: storage });
 
 router.get('/', trashCans);
 router.get('/image/:id', getImage);
 router.post('/', createTrashCan);
-router.post('/images', upload.single('image'), addImage);
+// router.post('/images', upload.single('image'), addImage);
 router.put('/:id', updateTrashCan);
 
 
