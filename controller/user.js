726a796f4d44a7a28e8fb9463d9a5629294cdfa9
Tung51056 <tung51056@donga.edu.vn>
2024-03-27 00:55:35 +0700

    "exipresin"

--- controller/user.js
+++ controller/user.js
@@ -48,7 +48,7 @@ const login = async (req, res) => {
             return res.status(400).json({ message: 'Password is incorrect' });
         }
 
-        const token = jwt.sign({ user_id: existingUser._id, email: existingUser.email, id: existingUser._id }, JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ user_id: existingUser._id, email: existingUser.email, id: existingUser._id }, JWT_SECRET, { expiresIn: '24h' });
 
         res.status(200).json({ user: { user_id: existingUser._id, user_name: existingUser.user_name, email: existingUser.email }, token });
 
@@ -76,7 +76,7 @@ const loginAdmin = async (req, res) => {
             return res.status(400).json({ message: 'Password is incorrect' });
         }
 
-        const token = jwt.sign({ email: existingUser.email, user_id: existingUser._id, role: existingUser.role }, JWT_SECRET, { expiresIn: '15s' });
+        const token = jwt.sign({ email: existingUser.email, user_id: existingUser._id, role: existingUser.role }, JWT_SECRET, { expiresIn: '12h' });
 
         res.status(200).json({ user: { user_id: existingUser._id, user_name: existingUser.user_name, email: existingUser.email }, token });
 
